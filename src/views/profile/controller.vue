722bba748059922f9a5f4346835c57fc4a86fa33
Pelom <1327016879@qq.com>
2022-08-09 22:25:23 +0800

    update profile

---
+++ src/views/profile/controller.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  option: string[]
+}>()
+</script>
+
+<template>
+<el-card>
+  <template #header>
+    <el-page-header
+      title="返回"
+      content="Spine"
+      @back="$emit('back')"
+    />
+  </template>
+  <el-row>
+    <el-col
+      :span="8"
+      v-for="opt in option"
+    >
+      <el-button
+        size="large"
+        @click="$emit('action', opt)"
+      >
+        {{ opt }}
+      </el-button>
+    </el-col>
+  </el-row>
+</el-card>
+</template>
+
+<style lang="scss" scoped>
+.el-card {
+  width: 40%;
+  height: 80%;
+  margin: auto 5% auto auto;
+  background: #fff8;
+  backdrop-filter: blur(5px);
+
+  &:deep(.el-card__header) {
+    background: #fffa;
+  }
+}
+
+.el-col {
+  padding: 5px;
+}
+
+.el-button {
+  width: 100%;
+}
+</style>
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-import { onMounted, onUnmounted } from 'vue'
+import controller from './controller.vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useStore, dataStore } from '@/store'
 import { load } from '@/axios/data'
@@ -14,9 +15,10 @@ data.ship = data.ship ?? await load('ui', 'ship')
 data.skin = data.ship ?? await load('ui', 'skin')
 const name = route.params.name as string
 const [name_cn, type, rarity, nationality] = data.ship[name]
+const option = ref([])
 
-const app = new Application()
-let container: HTMLElement
+const app = new Application({ resolution: 2 })
+let container: HTMLElement, back: Sprite, spine: Container, base: Sprite, char: Spine
 
 onMounted(() => {
   container = document.getElementById('pixi')
@@ -25,41 +27,64 @@ onMounted(() => {
   app.loader
     .add('back', `https://ui.al.pelom.cn/assets/shipbackground/${rarity}.png`)
     .add('char', `https://sd.al.pelom.cn/assets/spine/${name}/${name}.skel`)
-    .add('base', `https://ui.al.pelom.cn/assets/spinebase/${rarity > 10 ? rarity % 10 : rarity > 6 ? rarity - 2 : rarity}.png`)
+    .add('base', `https://ui.al.pelom.cn/assets/spinebase/${rarity > 6 ? rarity - 2 : rarity}.png`)
     .load((loader, resources) => {
-      const back = new Sprite(resources.back.texture)
+      back = new Sprite(resources.back.texture)
       back.anchor.set(0.5)
       back.scale.set(app.screen.width / back.width)
       back.position.set(app.screen.width / 2, app.screen.height / 2)
       app.stage.addChild(back)
 
-      const spine = new Container()
-      const base = new Sprite(resources.base.texture)
-      const char = new Spine(resources.char.spineData)
+      spine = new Container()
+      base = new Sprite(resources.base.texture)
+      char = new Spine(resources.char.spineData)
       spine.addChild(base)
       char.scale.set(0.5)
       char.position.set(spine.width / 2, spine.height - 25)
       spine.addChild(char)
       spine.position.set(160, app.screen.height - 100)
       app.stage.addChild(spine)
+      char.state.setAnimation(0, 'stand', true)
 
-      if (char.state.hasAnimation('stand'))
-        char.state.setAnimation(0, 'stand', true)
+      char.interactive = true
+      char.on('pointertap', () => {
+        base.visible = false
+        char.scale.set(1)
+        spine.position.set(app.screen.width / 4, app.screen.height / 2 + spine.height / 3)
+        char.state.setAnimation(0, 'normal', true)
+        option.value = char.state.data.skeletonData.animations.map((item) => {
+          return item.name
+        })
+      })
     })
 })
 
 onUnmounted(() => {
   app.destroy(true, true)
 })
+
+const handleBack = () => {
+  option.value = []
+  base.visible = true
+  char.scale.set(0.5)
+  spine.position.set(160, app.screen.height - 100)
+  char.state.setAnimation(0, 'stand', true)
+}
+
+const handleAction = (opt: string) => {
+  char.state.setAnimation(0, opt, true)
+}
 </script>
 
 <template>
-  <el-page-header
-    title="返回"
-    :content="name_cn"
-    @back="$router.push('/catalogue')"
-  />
   <div id="pixi"></div>
+  <div id="ui" v-show="option.length != 0">
+    <controller
+      :option="option as []"
+      @back="handleBack"
+      @action="handleAction"
+    />
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -69,7 +94,17 @@ onUnmounted(() => {
   right: 0;
   bottom: 0;
   left: 0;
-  background: gray;
   overflow: hidden;
+
+  &:deep(canvas) {
+    transform-origin: 0 0;
+    scale: 0.5;
+  }
+}
+
+#ui {
+  display: flex;
+  width: 100%;
+  height: 100%;
 }
 </style>
